1acf87a5914555889ebb315414eac9e8171010a1
osahatano-eng <osahatano@gmail.com>
2026-01-14 02:57:32 +0900

    Update page.tsx

--- app/p/[slug]/page.tsx
+++ app/p/[slug]/page.tsx
@@ -147,8 +147,8 @@ export default async function ProductPage({ params }: Props) {
                   <div style={{ marginTop: 14, fontSize: 12, opacity: 0.75, lineHeight: 1.75 }}>
                     ・再配布・転売は禁止<br />
                     ・商用利用OK（クレジット不要）<br />
-                    ・購入後の画像には透かし、クレジットは入りません。<br />
-                    ・高解像度データをダウンロードできます
+                    ・購入後の画像には透かし、クレジットは入りません<br />
+                    ・最適解データをダウンロードできます
                   </div>
 
                   <div style={{ marginTop: 12, fontSize: 12, opacity: 0.7, lineHeight: 1.6 }}>
@@ -226,3 +226,4 @@ export default async function ProductPage({ params }: Props) {
     </SiteFrame>
   );
 }
+
