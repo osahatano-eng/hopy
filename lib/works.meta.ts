801e6387ee534c360d56374836861e22d49a8b8e
osahatano-eng <osahatano@gmail.com>
2026-01-13 10:32:17 +0900

    Update works.meta.ts

--- lib/works.meta.ts
+++ lib/works.meta.ts
@@ -10,15 +10,16 @@ export const WORKS_META: Record<
     video?: string;
   }
 > = {
-  y8durmct: {
+  "y8durmct": {
     title: "y8durmct",
     price: 350,
     stripePriceId: "price_1Sov8RPdHZWs1X17dIDyEZPm",
   },
 
-  g4tywz0w: {
+  "g4tywz0w": {
     title: "g4tywz0w",
     price: 350,
     stripePriceId: "price_1SovAMPdHZWs1X17DxvgZ1Fg",
   },
 };
+
