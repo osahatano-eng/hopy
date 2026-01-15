0d4bcbf3ccd9e662f3d70d0382ad4e7694fa9ff9
osahatano-eng <osahatano@gmail.com>
2026-01-15 14:32:45 +0900

    Update works.meta.ts

--- lib/works.meta.ts
+++ lib/works.meta.ts
@@ -16,14 +16,15 @@ export const WORKS_META: Record<
   y8durmct: {
     title: "y8durmct",
     price: 390,
-    stripePriceId: "price_1SoqF5PdHZWs1X17cvujvG32",
+    stripePriceId: "price_1Sov8RPdHZWs1X17dIDyEZPm",
   },
 
   g4tywz0w: {
     title: "g4tywz0w",
     price: 390,
-    stripePriceId: "price_1SpcChPdHZWs1X17tkGdGAU5",
+    stripePriceId: "price_1SovAMPdHZWs1X17DxvgZ1Fg",
   },
 } as const;
+
 
 
