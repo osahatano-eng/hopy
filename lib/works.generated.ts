0d142e620077f5a61296798f25dd657b93fc2022
osahatano-eng <osahatano@gmail.com>
2026-01-21 15:12:01 +0900

    Update works.generated.ts

--- lib/works.generated.ts
+++ lib/works.generated.ts
@@ -20,93 +20,120 @@ export const WORKS: WorkBase[] = [
     image: "/works/g4tywz0w.png",
     downloadFile: "g4tywz0w.png",
   },
+  {
     slug: "5wf7bntd",
     image: "/works/5wf7bntd.png",
     downloadFile: "5wf7bntd.png",
   },
+  {
     slug: "r4xva4g5",
     image: "/works/r4xva4g5.png",
     downloadFile: "r4xva4g5.png",
   },
+  {
     slug: "m48opy86",
     image: "/works/m48opy86.png",
     downloadFile: "m48opy86.png",
   },
+  {
     slug: "wq7iwwsu",
     image: "/works/wq7iwwsu.png",
     downloadFile: "wq7iwwsu.png",
   },
+  {
     slug: "e1c8e0le",
     image: "/works/e1c8e0le.png",
     downloadFile: "e1c8e0le.png",
   },
+  {
     slug: "r8gor02s",
     image: "/works/r8gor02s.png",
     downloadFile: "r8gor02s.png",
   },
+  {
   　slug: "3nrqtvl7",
     image: "/works/3nrqtvl7.png",
     downloadFile: "3nrqtvl7.png",
   },
+  {
     slug: "37z1mdu6",
     image: "/works/37z1mdu6.png",
     downloadFile: "37z1mdu6.png",
   },
+  {
     slug: "6fapbktp",
     image: "/works/6fapbktp.png",
     downloadFile: "6fapbktp.png",
   },
+  {
     slug: "0vy0bibt",
     image: "/works/0vy0bibt.png",
     downloadFile: "0vy0bibt.png",
   },
+  {
     slug: "r6qvrdxz",
     image: "/works/r6qvrdxz.png",
     downloadFile: "r6qvrdxz.png",
   },
+  {
     slug: "8a24swce",
     image: "/works/8a24swce.png",
     downloadFile: "8a24swce.png",
   },
+  {
     slug: "0xl7t7xl",
     image: "/works/0xl7t7xl.png",
     downloadFile: "0xl7t7xl.png",
   },
+  {
     slug: "hlzcq1o6",
     image: "/works/hlzcq1o6.png",
     downloadFile: "hlzcq1o6.png",
   },
+  {
     slug: "7yknd8pc",
     image: "/works/7yknd8pc.png",
     downloadFile: "7yknd8pc.png",
   },
+  {
     slug: "hty8ut3e",
     image: "/works/hty8ut3e.png",
     downloadFile: "hty8ut3e.png",
   },
+  {
     slug: "up73h59u",
     image: "/works/up73h59u.png",
     downloadFile: "up73h59u.png",
   },
+  {
     slug: "949vogeu",
     image: "/works/949vogeu.png",
     downloadFile: "949vogeu.png",
   },
+  {
     slug: "ghlktp23",
     image: "/works/ghlktp23.png",
     downloadFile: "ghlktp23.png",
   },
+  {
     slug: "ofh3kwsi",
     image: "/works/ofh3kwsi.png",
     downloadFile: "ofh3kwsi.png",
   },
+  {
     slug: "ggu21lkq",
     image: "/works/ggu21lkq.png",
     downloadFile: "ggu21lkq.png",
   },
+  {
     slug: "j0writd2",
     image: "/works/j0writd2.png",
     downloadFile: "j0writd2.png",
+  },
+  {
+    slug: "vcjhum2r",
+    image: "/works/vcjhum2r.png",
+    downloadFile: "vcjhum2r.png",
   },
 ] as const;
 
