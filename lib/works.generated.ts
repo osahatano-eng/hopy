c4ddb2ab0c1b1149ac941310c4ec9083fc3ce119
osahatano-eng <osahatano@gmail.com>
2026-01-22 15:33:35 +0900

    Update works.generated.ts

--- lib/works.generated.ts
+++ lib/works.generated.ts
@@ -26,11 +26,6 @@ export const WORKS: WorkBase[] = [
     downloadFile: "5wf7bntd.png",
   },
   {
-    slug: "r4xva4g5",
-    image: "/works/r4xva4g5.png",
-    downloadFile: "r4xva4g5.png",
-  },
-  {
     slug: "m48opy86",
     image: "/works/m48opy86.png",
     downloadFile: "m48opy86.png",
